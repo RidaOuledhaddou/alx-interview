f343979d1c6d3e03a520dc546550a7ec1c290da1
RidaOuledhaddou <reda7ouledhaddou@gmail.com>
2024-08-20 11:05:25 +0000

    0x08-making_change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""
-	Making change O(n)
-"""
+"""Making change O(n)"""
 
 
 def makeChange(coins, total):
-    """	
-	Clasic Bottom-Up dynamic programming
-    """
+    """Clasic Bottom-Up dynamic programming"""
     temp_value = 0
     coins.sort(reverse=True)
 
